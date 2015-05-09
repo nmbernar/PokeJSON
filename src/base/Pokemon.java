f3fde2a0e52f676869d1276e7a4c26b5243596d5
nmbernar <nmbernar@ncsu.edu>
2015-05-09 14:26:16 -0400

    Pokemon and Type classes

---
+++ src/base/Pokemon.java
@@ -0,0 +1,201 @@
+package base;
+
+public class Pokemon {
+	private String name;
+	private int dexNumber;
+	private Type primaryType;
+	private Type secondaryType;
+	private int total;
+	private int hp;
+	private int att;
+	private int def;
+	private int spatt;
+	private int spdef;
+	private int spd;
+	private float height;
+	private float weight;
+	private int capRate;
+	private int expGrowth;
+	private int evs;
+	
+	public Pokemon(String name, int num, Type type1, Type type2, int total, int hp, int att, int def, int spatt, int spdef, int spd){
+		this.name = name;
+		this.dexNumber = num;
+		this.primaryType = type1;
+		this.secondaryType = type2;
+		this.total = total;
+		this.hp = hp;
+		this.att = att;
+		this.def = def;
+		this.spatt = spatt;
+		this.spdef = spdef;
+		this.spd = spd;
+	}
+
+
+	public Pokemon() {
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getDexNumber() {
+		return dexNumber;
+	}
+
+
+	public void setDexNumber(int dexNumber) {
+		this.dexNumber = dexNumber;
+	}
+
+
+	public Type getPrimaryType() {
+		return primaryType;
+	}
+
+
+	public void setPrimaryType(Type primaryType) {
+		this.primaryType = primaryType;
+	}
+
+
+	public Type getSecondaryType() {
+		return secondaryType;
+	}
+
+
+	public void setSecondaryType(Type secondaryType) {
+		this.secondaryType = secondaryType;
+	}
+
+
+	public int getTotal() {
+		return total;
+	}
+
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+
+	public int getHp() {
+		return hp;
+	}
+
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+
+	public int getAtt() {
+		return att;
+	}
+
+
+	public void setAtt(int att) {
+		this.att = att;
+	}
+
+
+	public int getDef() {
+		return def;
+	}
+
+
+	public void setDef(int def) {
+		this.def = def;
+	}
+
+
+	public int getSpatt() {
+		return spatt;
+	}
+
+
+	public void setSpatt(int spatt) {
+		this.spatt = spatt;
+	}
+
+
+	public int getSpdef() {
+		return spdef;
+	}
+
+
+	public void setSpdef(int spdef) {
+		this.spdef = spdef;
+	}
+
+
+	public int getSpd() {
+		return spd;
+	}
+
+
+	public void setSpd(int spd) {
+		this.spd = spd;
+	}
+
+
+	public float getHeight() {
+		return height;
+	}
+
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+
+	public float getWeight() {
+		return weight;
+	}
+
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+
+	public int getCapRate() {
+		return capRate;
+	}
+
+
+	public void setCapRate(int capRate) {
+		this.capRate = capRate;
+	}
+
+
+	public int getExpGrowth() {
+		return expGrowth;
+	}
+
+
+	public void setExpGrowth(int expGrowth) {
+		this.expGrowth = expGrowth;
+	}
+
+
+	public int getEvs() {
+		return evs;
+	}
+
+
+	public void setEvs(int evs) {
+		this.evs = evs;
+	}
+	
+	
+}
+
---
+++ src/base/Type.java
@@ -0,0 +1,8 @@
+package base;
+
+public enum Type {
+	Bug, Dark, Dragon, Electric, Fairy, 
+	Fighting, Fire, Flying, Ghost, Grass, 
+	Ground, Ice, Normal, Poison, Psychic, 
+	Rock, Steel, Water;
+}
